569387bf4e35da0c26edb0ac24ee6c3212570f87
pranavr2003 <rsaipranav2003@gmail.com>
2021-07-21 05:09:49 +0000

    test changes

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import streamlit as st
 
-# image = Image.open('https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3VucmlzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')
-st.image(image='https://file-examples-com.github.io/uploads/2020/03/file_example_SVG_20kB.svg')
+# st.image('https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3VucmlzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')
+st.image(image='https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Tottenham_Hotspur.svg/1200px-Tottenham_Hotspur.svg.png')
+st.image(image='test.svg.png')
