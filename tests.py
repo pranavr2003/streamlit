0e1a6781b9fdd745be709465033d070b7683eb44
pranavr2003 <rsaipranav2003@gmail.com>
2021-07-21 04:24:59 +0000

    tests run issues encountered

---
+++ tests.py
@@ -0,0 +1,5 @@
+from PIL import Image
+import streamlit as st
+
+# image = Image.open('https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3VucmlzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')
+st.image(image='https://file-examples-com.github.io/uploads/2020/03/file_example_SVG_20kB.svg')
