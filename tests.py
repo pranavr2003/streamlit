90a686f940c901143d983fcb6ef8f4a119802786
pranavr2003 <rsaipranav2003@gmail.com>
2021-07-21 05:18:16 +0000

    tests run

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import streamlit as st
 
-# st.image('https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3VucmlzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')
-st.image(image='https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Tottenham_Hotspur.svg/1200px-Tottenham_Hotspur.svg.png')
-st.image(image='test.svg.png')
+st.image('https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3VucmlzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')
+# st.image(image='https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Tottenham_Hotspur.svg/1200px-Tottenham_Hotspur.svg.png')
+# st.image(image='test.svg')
+# st.image(image='https://i.pinimg.com/originals/79/bd/00/79bd00c9e1ab7e30242bc47c25689e91.jpg')
