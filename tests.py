cdf64e74cfc4db72684910e59d8dd3b28143297f
pranavr2003 <rsaipranav2003@gmail.com>
2021-08-04 08:35:02 +0000

    tests done, vdonato pinged

--- tests.py
+++ tests.py
@@ -14,6 +14,7 @@ import streamlit as st
 
 # image = Image.open('https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3VucmlzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')
 
-st.image('https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3VucmlzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')
+# st.image('test.svg')
+st.image('test.svg', width=20)
 
 
